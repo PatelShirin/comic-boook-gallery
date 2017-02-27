6dac581365d8d8c569294eadb3406ff4706cb1e2
ShirinPatel <ShirinPatel@ACER>
2017-02-26 22:56:41 -0500

    Minor changes

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,31 +8,31 @@ using System.Web.Mvc;
 
 namespace ComicBookGallery.Controllers
 {
-    public class ComicBooksController : Controller
-    {
-        private ComicBookRepository _comicBookRepository = null;
+	public class ComicBooksController : Controller
+	{
+		private ComicBookRepository _comicBookRepository = null;
 
-        public ComicBooksController()
-        {
-            _comicBookRepository = new ComicBookRepository();
-        }
+		public ComicBooksController()
+		{
+			_comicBookRepository = new ComicBookRepository();
+		}
 
-        public ActionResult Index()
-        {
-            var comicBooks = _comicBookRepository.GetComicBooks();
-            return View(comicBooks);
-        }
+		public ActionResult Index()
+		{
+			var comicBooks = _comicBookRepository.GetComicBooks();
+			return View(comicBooks);
+		}
 
-        public ActionResult Detail(int? id)
-        {
-            if(id == null)
-            {
-                return HttpNotFound();
-            }
+		public ActionResult Detail(int? id)
+		{
+			if(id == null)
+			{
+				return HttpNotFound();
+			}
 
-            var comicBook = _comicBookRepository.GetComicBook((int)id);
+			var comicBook = _comicBookRepository.GetComicBook((int)id);
 
-            return View(comicBook);
-        }
-    }
+			return View(comicBook);
+		}
+	}
 }
